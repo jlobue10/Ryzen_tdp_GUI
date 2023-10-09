6a0a6eb9f47c7c5a03ed53dceaf150afd0dedfb6
jlobue10 <jlobue10@gmail.com>
2023-10-08 21:10:13 -0700

    Add files via upload

---
+++ GUI/src/mainwindow.h
@@ -0,0 +1,50 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+using std::string;
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_tdp_Slider_valueChanged(int value);
+
+    void on_GPU_Clock_Slider_valueChanged(int value);
+
+    void on_tdp_Apply_pushButton_clicked();
+
+    void on_GPU_Apply_pushButton_clicked();
+
+    void on_Smoke_checkBox_toggled(bool checked);
+
+    void on_Boost_checkBox_toggled(bool checked);
+
+    void on_tdp_lineEdit_editingFinished();
+
+    void on_GPU_Clock_lineEdit_editingFinished();
+
+    void readSettings();
+
+    void writeSettings();
+
+    void Ryzen_tdp_command(string Ryzen_command);
+
+    void on_About_pushButton_clicked();
+
+    void on_updateButton_Clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
