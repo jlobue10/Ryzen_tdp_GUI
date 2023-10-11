b55bb979b389ca3b1e11f10a3d5cd814c9271a32
jlobue10 <jlobue10@gmail.com>
2023-10-10 22:47:33 -0700

    Update Ryzen_tdp_GUI_install.sh

--- Ryzen_tdp_GUI_install.sh
+++ Ryzen_tdp_GUI_install.sh
@@ -26,7 +26,8 @@ NOBARA=$?
 
 if [ $FEDORA_BASE == 0 ]; then
 	echo -e '\nFedora based installation starting.\n'
-		sudo dnf install cmake gcc-c++ qt6-qtbase-devel qt6-qttools-devel qt5-qtbase-devel qt5-qttools-devel qt5-linguist
+		sudo dnf install cmake gcc-c++ qt5-qtbase-devel qt5-qttools-devel qt5-linguist
+  		#sudo dnf install qt6-qtbase-devel qt6-qttools-devel 
 fi
 
 which apt 2>/dev/null
