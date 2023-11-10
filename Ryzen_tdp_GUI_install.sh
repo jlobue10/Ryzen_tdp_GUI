9d86b59211cde002a0aaadc043cd07d578e6d361
jlobue10 <jlobue10@gmail.com>
2023-11-10 09:43:46 -0800

    Update Ryzen_tdp_GUI_install.sh

--- Ryzen_tdp_GUI_install.sh
+++ Ryzen_tdp_GUI_install.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 # A simple script to install the rEFInd customization GUI
 
-echo ""
-
-read -p "Please make sure a user password is already set before continuing. If you have not set the user\
- or sudo password, please exit this installer with 'Ctrl+c' and then create a password either using 'passwd'\
- from a command line or by using the KDE Plasma User settings GUI. Otherwise, press Enter/Return to continue with the install."
-
 cd $HOME
 rm -rf ./Ryzen_tdp_GUI/
 git clone https://github.com/jlobue10/Ryzen_tdp_GUI
@@ -87,17 +81,6 @@ EOF
 
 chmod +x $HOME/.local/Ryzen_tdp_GUI/GUI/Ryzen_tdp_GUI.desktop 2>/dev/null
 sudo cp $HOME/.local/Ryzen_tdp_GUI/GUI/Ryzen_tdp_GUI.desktop /usr/share/applications 2>/dev/null
-
-while true; do
-	echo ""
-	read -p "Do you want to copy the Ryzen tdp GUI icon to the desktop? (y/n) " YN
-	case $YN in
-		[yY]) echo -e "\nOk, icon will be copied to the desktop.\n"
-			cp $HOME/.local/Ryzen_tdp_GUI/GUI/Ryzen_tdp_GUI.desktop $HOME/Desktop
-			chmod +x $HOME/Desktop/Ryzen_tdp_GUI.desktop
-			break;;
-		[nN]) echo -e "\nIcon will not be copied to the desktop.\n"
-			exit;;
-		*) echo -e "\nInvalid response.";;
-	esac
-done
+cp $HOME/.local/Ryzen_tdp_GUI/GUI/Ryzen_tdp_GUI.desktop $HOME/Desktop
+chmod +x $HOME/Desktop/Ryzen_tdp_GUI.desktop
+			
