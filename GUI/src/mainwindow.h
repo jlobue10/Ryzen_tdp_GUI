398e9cb27d22bf2162244873830a30c7a6af3fcc
jlobue10 <jlobue10@gmail.com>
2023-12-06 10:37:45 -0800

    Update mainwindow.h

--- GUI/src/mainwindow.h
+++ GUI/src/mainwindow.h
@@ -19,6 +19,26 @@ public:
     void Ryzen_tdp_command(string Ryzen_command);
     string Get_tdp_Info(const char* tdp_type);
 
+    typedef enum rc71l_platform_mode {
+        rc71l_platform_mode_hidraw,
+        rc71l_platform_mode_asus_mcu,
+    } rc71l_platform_mode_t;
+
+    typedef struct rc71l_platform {
+        struct udev *udev;
+
+        rc71l_platform_mode_t platform_mode;
+
+        unsigned long mode;
+        unsigned int modes_count;
+    } rc71l_platform_t;
+
+    typedef struct Find_MCU_Mode {
+        rc71l_platform_t platform;
+    } Find_MCU_Mode_t;
+
+   Find_MCU_Mode_t Find_MCU_Mode_test;
+
 private slots:
     void on_tdp_Slider_valueChanged(int value);
 
@@ -49,6 +69,10 @@ private slots:
     void on_updateButton_Clicked();
 
     void on_GPU_Clock_checkBox_stateChanged(int arg1);
+
+    string find_device(struct udev *udev);
+
+    int init_platform(rc71l_platform_t *const platform);
 
     void update_MCU_Mode_lineEdit();
 
